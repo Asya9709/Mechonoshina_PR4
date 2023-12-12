536ba1919e5391f0f4fef294510e1c7dd63ca583
Asya9709 <asyabekker006@gmail.com>
2023-12-12 22:54:44 +0500

    Update №1.php

--- №1.php
+++ №1.php
@@ -1,9 +1,26 @@
 function convertToSquareMeters(a)
 {
-    b = a * 1.0925;
-    c = b * 10000;
-    return c;
+b = a * 1.0925;
+c = b * 10000;
+return c;
+}
+function convertToOtherUnits(c, d)
+{
+switch (d) {
+case 'сотки':
+return c * 0.01;
+case 'югер':
+return c * 0.0001;
+case 'акр':
+return c * 0.000247105;
+case 'площадь Земли':
+return c / 510100000;
+default:
+return c;
+}
 }
 a = 5;
 c = convertToSquareMeters(a);
-echo "{a} десятин равны {c} квадратным метрам.";
+f = 'сотки';
+g = convertToOtherUnits(c, f);
+echo "{a} десятин равны {g} {f}.";
